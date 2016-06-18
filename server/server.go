7614afd26b5d44a6d8b8d9efdfeb37b417188fcb
Evan Kroske <evankroske@google.com>
2016-06-17 21:02:35 -0700

    Server: Logs content length of requests

--- server/server.go
+++ server/server.go
@@ -2,12 +2,18 @@ package main
 
 import (
   "flag"
+  "net/http"
   "log"
 )
 
-var port = flag.Int("port", 8080, "TCP port on which to listen")
+var addr = flag.String("addr", ":8080", "Address at which to listen")
 
 func main() {
   flag.Parse()
-  log.Printf("Listening on port %d", *port)
+  log.Printf("Listening at address %v", *addr)
+
+  http.HandleFunc("/signin", func(w http.ResponseWriter, r *http.Request) {
+    log.Println(r.ContentLength)
+  })
+  log.Fatalln(http.ListenAndServe(*addr, nil))
 }
