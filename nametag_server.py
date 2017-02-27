79b928e08f6934d710a37eb5e329c55b02dd78ca
Swathi Lal <swathilalx@gmail.com>
2017-02-26 19:56:18 -0800

    Saving received file and writing user information to csv.
    Fixes #7

--- nametag_server.py
+++ nametag_server.py
@@ -1,4 +1,4 @@
-import os
+import os, base64, csv, datetime
 from flask import Flask, request, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 
@@ -13,16 +13,33 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+def save_user_info(name, email):
+    needs_header = False
+    if not os.path.exists("userInformation.csv"):
+        needs_header = True
+
+    with open("userInformation.csv", 'a') as csvfile:
+        fieldnames = ["Name", "Email", "Timestamp"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        if needs_header:
+            writer.writeheader()
+        writer.writerow({"Name":name, "Email":email, "Timestamp":datetime.datetime.now()})
+
+
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
-
 
 
 @app.route('/print', methods=['POST', 'GET'])
 def upload_file():
     if request.method == 'POST':
         print ("'name = '{}' email = '{}' nametag_img = '{}'".format( str(request.form['name']), str(request.form['email']), str(request.form['nametag_img']) ) )
+        with open("temp.png", "wb") as f:
+            # Removing the prefix 'data:image/png;base64,'
+            data = request.form['nametag_img'].split(",")[1]
+            f.write(base64.b64decode(data))
+        save_user_info(request.form['name'], request.form['email'])
         return "OK"
     else:
         return "Working"
