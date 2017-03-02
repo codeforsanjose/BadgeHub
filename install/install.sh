92ca7fc7a9c2b2f0d474296e70781c07515c6772
Kyle Falconer <kfalconer@gmail.com>
2017-03-02 08:43:15 +0000

    Fixed install script
    renamed install_deps.sh to install.sh
    corrected typos in the autostart script
    added pip install commands to install.sh
    added some safety checks and cleanup to install.sh

---
+++ install/install.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+#########################################################
+#              Install script for the                   #
+#                Code For San Jose                      #
+#                  Login System                         #
+#  https://github.com/codeforsanjose/CFSJ-Login-System  #            
+#                                                       #
+#               (run this file once)                    #
+#########################################################
+
+
+PROJECT_DIR=`pwd`
+
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y --force-yes \
+	zbar-tools \
+	qrencode \
+	libcups2-dev \
+	libcupsimage2-dev  \
+	g++ \
+	cups \
+	cups-client
+
+
+# install the printer driver for the Dymo LabelWriter 450
+# for information about this, see the documentation on the
+# dymo website for the Linux SDK and CUPS Drivers
+wget http://download.dymo.com/Download%20Drivers/Linux/Download/dymo-cups-drivers-1.4.0.tar.gz
+tar xvf dymo-cups-drivers-1.4.0.tar.gz
+cd dymo-cups-drivers-1.4.0.5
+sudo ./configure
+sudo make
+sudo make install
+cd $PROJECT_DIR
+rm -rf cd dymo-cups-drivers-1.4.0.5
+rm dymo-cups-drivers-1.4.0.tar.gz
+sudo usermod -a -G lpadmin pi
+
+
+# install Flask and other Python dependencies
+sudo pip install -r requirements.txt
+
+
+# copy the startup scripts to allow Chromium and the login
+# system to start automatically at boot
+PI_CONFIG_DIR="/home/pi/.config/lxsession/LXDE-pi"
+if [ ! -f $PI_CONFIG_DIR"/autostart" ]; then
+    cp $PI_CONFIG_DIR"/autostart" $PI_CONFIG_DIR"/autostart.orig"
+fi
+
+cp ./install/autostart $PI_CONFIG_DIR"/autostart"
+
+
+echo "Finished installing!"
+echo "Please set up your printer now at http://localhost:631"
+exit 0
